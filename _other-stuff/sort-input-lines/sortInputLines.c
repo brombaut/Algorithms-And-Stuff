5f5fef4fea6350d15600e8c3fe8533b7ef5042c5
brombaut <ben.rombaut@unb.ca>
2019-08-09 19:01:22 -0300

    Starting sort input lines

---
+++ _other-stuff/sort-input-lines/sortInputLines.c
@@ -0,0 +1,33 @@
+/**
+ * Exercise from The C Programming Language
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000  /* max #lines to be sorted */
+char *lineptr[MAXLINES];  /* pointers to text lines */
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(void *lineptr[], int leftm int right, int (*comp)(void *, void *));
+int numcmp(char *, char *);
+
+/* sort input lines */
+int main(int argc, *argv[]) {
+    int nlines;  /* number of input lines read */
+    int numeric = 0;  /* 1 if numeric sort */
+
+    if (argc > 1 && strcmp(argv[1], "-n") == 0) {
+        numeric = 1;
+    }
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        qsort((void **) lineptr, 0, nlines - 1, (int (*)(void*, void*))(numeric ? nump : strcmp));
+        writelines(lineptr, nlines);
+        return 0;
+    } else {
+        printf("input too big to sort\n");
+        return 1;
+    }
+}
