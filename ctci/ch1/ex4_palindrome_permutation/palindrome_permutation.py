7ba13fc66c09c8f205b2d6587de68a6f3e35be48
brombaut <ben.rombaut@unb.ca>
2019-09-18 20:09:31 -0300

    Palindrom Permutation

---
+++ ctci/ch1/ex4_palindrome_permutation/palindrome_permutation.py
@@ -0,0 +1,30 @@
+
+# Returns true if a string has all unique characters
+def palindrome_permutation(string):
+    no_spaces = ""
+    for char in string.lower():
+        if char != " ":
+            no_spaces += char
+    sorted_arr = sorted(no_spaces)
+    i = 0
+    found_single = False
+    while i < len(sorted_arr):
+        if i == len(sorted_arr) - 1 and found_single:
+            return False
+        if sorted_arr[i] != sorted_arr[i+1]:
+            if found_single:
+                return False
+            else:
+                found_single = True
+                i += 1
+                continue
+        i += 2
+    return True
+
+
+if __name__== "__main__":
+    if palindrome_permutation("Tact Coaz"):
+        print("Is Palindrom Permutation")
+    else:
+        print("Is Not Palindrom Permutation")
+
