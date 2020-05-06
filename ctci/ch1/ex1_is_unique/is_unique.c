258474ed7e59cbf83e683f3167b8564dd099d08c
Ben <ben.rombaut@eigen.io>
2020-05-06 11:34:53 -0300

    Just commiting to switch computers

---
+++ ctci/ch1/ex1_is_unique/is_unique.c
@@ -0,0 +1,17 @@
+
+
+char isUnique(char *s) {
+  char c = *s;
+  while(c) {
+    printf("%c", c);
+  }
+
+  return 0;
+}
+
+main(char *s) {
+  char c = *s;
+  while(c) {
+    printf("%c", c);
+  }
+}
