6c6508d38b20fda7bec650b457904c864f5695a7
brombaut <ben.rombaut@unb.ca>
2020-05-06 12:02:34 -0300

    Is unique C version

--- ctci/ch1/ex1_is_unique/is_unique.c
+++ ctci/ch1/ex1_is_unique/is_unique.c
@@ -1,17 +1,27 @@
-
+#include <stdio.h>
 
 char isUnique(char *s) {
-  char c = *s;
-  while(c) {
-    printf("%c", c);
-  }
-
-  return 0;
+    int charArray[26];
+    for (int i=0; i < 26; ++i) {
+        charArray[i] = 0;
+    }
+    char c = *s++;
+    while(c) {
+        if (charArray['z' - c]) {
+            return 0;
+        }
+        charArray['z' - c] = 1;
+        c = *s++;
+    }
+    return 1;
 }
 
-main(char *s) {
-  char c = *s;
-  while(c) {
-    printf("%c", c);
-  }
+int main() {
+    char result = isUnique("tesi");
+    if (result) {
+        printf("%s\n", "Is Unique");
+    } else {
+        printf("%s\n", "Is Not Unique");
+    }
+    return 0;
 }
