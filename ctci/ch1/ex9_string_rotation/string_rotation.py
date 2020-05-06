437127d5b8709f7bd22c37bcb25fc3b311a66666
Ben <ben.rombaut@eigen.io>
2020-05-05 21:36:24 -0300

    String rotation

--- ctci/ch1/ex9_string_rotation/string_rotation.py
+++ ctci/ch1/ex9_string_rotation/string_rotation.py
@@ -1,14 +1,32 @@
 import unittest
 
+def is_substring(s1, s2):
+    return s2 in s1
 
-def string_compression(s):
-    pass
+def string_rotation(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    if s1 == s2:
+        return True
+    concattedString = s1 + s1
+    return is_substring(concattedString, s2)
 
 
-class TestStringCompression(unittest.TestCase):
+class TestStringRotation(unittest.TestCase):
 
-    def test_fail(self):
-        self.assertTrue(1 == 0)
+    def test_different_string_lengths(self):
+        self.assertFalse(string_rotation("water", "waters"))
+        self.assertFalse(string_rotation("waters", "water"))
+        self.assertFalse(string_rotation("", "water"))
+        self.assertFalse(string_rotation("water", ""))
+    
+    def test_same_string(self):
+        self.assertTrue(string_rotation("water", "water"))
+        self.assertTrue(string_rotation("", ""))
+
+    def test_rotated_string(self):
+        self.assertTrue(string_rotation("water", "terwa"))
+        self.assertTrue(string_rotation("waterbottle", "erbottlewat"))
 
 
 if __name__== "__main__":
