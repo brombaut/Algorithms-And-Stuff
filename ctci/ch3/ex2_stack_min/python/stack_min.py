17c88cc89ee189294d0708f10082b2d81d972916
Ben Rombaut <rombaut.benj@gmail.com>
2020-07-21 08:52:46 -0300

    Min stack

--- ctci/ch3/ex2_stack_min/python/stack_min.py
+++ ctci/ch3/ex2_stack_min/python/stack_min.py
@@ -1,14 +1,52 @@
 import unittest
 
+
+class StackNode:
+    def __init__(self, value, minimum):
+        self.value = value
+        self.minimum = minimum
+
+
+class StackMin:
+    def __init__(self):
+        self.stack = []
+        self.index = 0
+
+    def push(self, value):
+        currMin = self.minimum()
+        if value < currMin:
+            currMin = value
+        node = StackNode(value, currMin)
+        self.stack.insert(self.index, node)
+        self.index += 1
+
+    def pop(self):
+        self.index -= 1
+        return self.stack[self.index].value
+
+    def peek(self):
+        return self.stack[self.index - 1].value if self.index > 0 else 9999
+
+    def minimum(self):
+        return self.stack[self.index - 1].minimum if self.index > 0 else 9999
+
+
 def stack_min():
     pass
 
 
 class TestStackMin(unittest.TestCase):
 
-    def test_fail(self):
-        self.assertTrue(1 == 0)
+    def test_stack_min(self):
+        stack = StackMin()
+        stack.push(4)
+        stack.push(7)
+        self.assertEqual(stack.minimum(), 4)
+        stack.push(2)
+        self.assertEqual(stack.minimum(), 2)
+        stack.pop()
+        self.assertEqual(stack.minimum(), 4)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     unittest.main(verbosity=2)
