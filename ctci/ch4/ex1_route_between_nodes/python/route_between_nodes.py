434a148d4391384e1bb2d4f904543a428aa9326b
Ben Rombaut <rombaut.benj@gmail.com>
2020-07-29 11:32:40 -0300

    Route between nodes

--- ctci/ch4/ex1_route_between_nodes/python/route_between_nodes.py
+++ ctci/ch4/ex1_route_between_nodes/python/route_between_nodes.py
@@ -1,14 +1,128 @@
 import unittest
+import collections
+from enum import Enum
 
-def route_between_nodes():
+
+class State(Enum):
+    UNVISITED = 0
+    VISITED = 1
+    VISITING = 2
+
+
+class Node:
+    def __init__(self, name):
+        self._name = name
+        self._children = []
+        self._state = State.UNVISITED
+
+    def name(self):
+        return self._name
+
+    def adjacent_nodes(self):
+        return self._children
+
+    def addChild(self, child):
+        self._children.append(child)
+
+    def connect(self, child):
+        self._children.append(child)
+        child.addChild(self)
+
+    def state(self):
+        return self._state
+
+    def unvisited(self):
+        self._state = State.UNVISITED
+
+    def visiting(self):
+        self._state = State.VISITING
+
+    def visited(self):
+        self._state = State.VISITED
+
+
+class Graph:
+    def __init__(self, nodes):
+        self._nodes = nodes
+
+    def get_node_by_name(self, name):
+        for node in self._nodes:
+            if node.name() == name:
+                return node
+        return None
+
+    def route_between_nodes(self, start, end):
+        if start == end:
+            return True
+        if start is None or end is None:
+            return False
+
+        for n in self._nodes:
+            n.unvisited()
+        q = collections.deque([])
+        start.visiting()
+        q.append(start)
+        while(len(q) > 0):
+            u = q.popleft()
+            if u is not None:
+                for v in u.adjacent_nodes():
+                    if v.state() == State.UNVISITED:
+                        if v == end:
+                            return True
+                        else:
+                            v.visiting()
+                            q.append(v)
+                u.visited()
+        return False
+
+
+def route_between_nodes(graph, node_a, node_b):
     pass
+
+
+def build_directed_graph():
+    node_0 = Node("0")
+    node_1 = Node("1")
+    node_2 = Node("2")
+    node_3 = Node("3")
+    node_4 = Node("4")
+    node_5 = Node("5")
+    node_6 = Node("6")
+
+    node_0.addChild(node_1)
+    node_1.addChild(node_2)
+    node_2.addChild(node_0)
+    node_2.addChild(node_3)
+    node_3.addChild(node_2)
+    node_4.addChild(node_6)
+    node_5.addChild(node_4)
+    node_6.addChild(node_5)
+
+    return Graph([
+        node_0,
+        node_1,
+        node_2,
+        node_3,
+        node_4,
+        node_5,
+        node_6,
+    ])
 
 
 class TestRouteBetweenNodes(unittest.TestCase):
 
-    def test_fail(self):
-        self.assertTrue(1 == 0)
+    def test_route_exists(self):
+        g = build_directed_graph()
+        n0 = g.get_node_by_name("0")
+        n3 = g.get_node_by_name("3")
+        self.assertTrue(g.route_between_nodes(n0, n3))
+
+    def test_route_does_not_exists(self):
+        g = build_directed_graph()
+        n0 = g.get_node_by_name("2")
+        n3 = g.get_node_by_name("5")
+        self.assertFalse(g.route_between_nodes(n0, n3))
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     unittest.main(verbosity=2)
